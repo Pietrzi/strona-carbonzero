c40667bb565ba8f5a9560fee5a72731fc53a79fc
Pietrzi <pietrzi@op.pl>
2020-09-11 14:41:55 +0200

    started styling some navbar and home

--- strona-carbonz/src/App.js
+++ strona-carbonz/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './scss/style.scss';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 import About from './components/About';
--- strona-carbonz/src/components/Home.js
+++ strona-carbonz/src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Engage from './Engage';
+import logo from '../assetes/CarbonZero-logo.png';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home-wrapper">
+            <div className="home-logo">
+                <img src={logo} alt="logo CarbonZero"/>
+            </div>
             <Engage />
         </div>
     )
