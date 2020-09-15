d527617d6655eb510a1ca5a38ee3b991937a8fb8
Pietrzi <pietrzi@op.pl>
2020-09-15 19:18:07 +0200

    Updates

---
+++ precache-manifest.d4058005e4e3dba264bca71ee80b488e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0bdf104645fde37af5b6eb5cd1b5d2",
+    "url": "/strona-carbonzero/index.html"
+  },
+  {
+    "revision": "78c7aaa58262a738b50a",
+    "url": "/strona-carbonzero/static/css/main.811a8ed1.chunk.css"
+  },
+  {
+    "revision": "902445d88079a7e83d54",
+    "url": "/strona-carbonzero/static/js/2.62bb38b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/strona-carbonzero/static/js/2.62bb38b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c7aaa58262a738b50a",
+    "url": "/strona-carbonzero/static/js/main.a8927573.chunk.js"
+  },
+  {
+    "revision": "fd15f0deec75de1a1515",
+    "url": "/strona-carbonzero/static/js/runtime-main.3eb6d208.js"
+  },
+  {
+    "revision": "d0da3b74b0210346212d0ac96dae4ef9",
+    "url": "/strona-carbonzero/static/media/CarbonZero-logo.d0da3b74.png"
+  }
+]);
