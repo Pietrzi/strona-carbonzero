4b60954e1bdc11cb1ef9e57a9ad016a1132aa0b5
Pietrzi <pietrzi@op.pl>
2020-10-01 13:03:11 +0200

    info section

--- strona-carbonz/src/components/Info.js
+++ strona-carbonz/src/components/Info.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Info = () => {
     return (
-        <div className="info-container">
-            <h2>Info, miejsce na adresy, dane firmy etc.</h2>
-        </div>
+        <section className="info-container">
+            <div className="info-section-box">
+                <p className="info-title">Dane kontaktowe</p>
+                <p className="info-name">EKO SERWIS Szymon Barciński</p>
+                <p className="info-text">ul. Młodzieżowa 31P/15</p>
+                <p className="info-text margbot">09-100 Płońsk</p>
+                <p className="info-text"> NIP: 567-171-10-09</p>
+                <p className="info-text margbot">REGON: 367897610</p>
+                <p className="info-text margbot">e-mail: biuro@carbonzero.pl</p>
+                <p className="info-text">POMPY CIEPŁA I REKUPERACJA: +48 603 756 412</p>
+                <p className="info-text">SERWIS: +48 608 387 064</p>
+            </div>
+            <div className="info-section-box altalign">
+                <p className="info-title">Przejdź do</p>
+                <NavLink to='/'><p>Home</p></NavLink>
+                <NavLink to='/onas'><p>O nas</p></NavLink>
+                <NavLink to='/oferta'><p>Oferta</p></NavLink>
+                <NavLink to='/galeria'><p>Galeria</p></NavLink>
+                <NavLink to='/kontakt'><p>Kontakt</p></NavLink>
+            </div>
+            <div className="info-section-box altalign">
+                <p className="info-title">Dołącz do nas</p>
+                <p className="info-text"> ikonki social media</p>
+            </div>
+        </section>
     )
 }
 
--- strona-carbonz/src/components/Partners.js
+++ strona-carbonz/src/components/Partners.js
@@ -5,16 +5,16 @@ const Partners = () => {
         <section className="partners-section">
             <h2>Nasza firma współpracuje z takimi partnerami jak:</h2>
             <div className="logos-container">
-            <img src={require('../assets/comp1.png')} alt="logo"/>
-            <img src={require('../assets/comp2.png')} alt="logo"/>
-            <img src={require('../assets/comp3.png')} alt="logo"/>
-            <img src={require('../assets/comp4.png')} alt="logo"/>
-            <img src={require('../assets/comp5.png')} alt="logo"/>
-            <img src={require('../assets/comp6.png')} alt="logo"/>
-            <img src={require('../assets/comp7.png')} alt="logo"/>
-            <img src={require('../assets/comp8.png')} alt="logo"/>
-            <img src={require('../assets/comp9.png')} alt="logo"/>
-            <img src={require('../assets/comp10.png')} alt="logo"/>
+            <img src={require('../assets/comp1.png')} alt="logo1" className="logo1"/>
+            <img src={require('../assets/comp2.png')} alt="logo2" className="logo2"/>
+            <img src={require('../assets/comp3.png')} alt="logo3" className="logo3"/>
+            <img src={require('../assets/comp4.png')} alt="logo4" className="logo4"/>
+            <img src={require('../assets/comp5.png')} alt="logo5" className="logo5"/>
+            <img src={require('../assets/comp6.png')} alt="logo6" className="logo6"/>
+            <img src={require('../assets/comp7.png')} alt="logo7" className="logo7"/>
+            <img src={require('../assets/comp8.png')} alt="logo8" className="logo8"/>
+            <img src={require('../assets/comp9.png')} alt="logo9" className="logo9"/>
+            <img src={require('../assets/comp10.png')} alt="logo10" className="logo10"/>
             </div>
         </section>
     )
