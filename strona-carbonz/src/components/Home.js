b702d3565d991859b4bc87bba31fc7f60c0625ed
Pietrzi <pietrzi@op.pl>
2020-12-09 11:02:41 +0100

    some text changing

--- strona-carbonz/src/components/Home.js
+++ strona-carbonz/src/components/Home.js
@@ -16,10 +16,12 @@ const Home = () => {
                 <div className="about-text-holder">
                     {/* <p className="about-title">O nas</p> */}
                     <p className="about-text">Od ponad 7 lat CarbonZero promuje rozwiązania przyjazne środowisku i  użytkownikowi.</p>
-                    <p className="about-text">Również logo CarbonZero wyłania się z czerni symbolizującej węgiel i zanieczyszczone powietrze.</p>
-                    <p className="about-text">Tworzymy rozwiązania, które ograniczają koszty ogrzewania i dbają o czyste powietrze w Państwa domach.</p>
                     <p className="about-text">Nasz Zespół to LUDZIE z wieloletnim doświadczeniem i ogromną pasją tworzenia nowoczesnych i bezpiecznych rozwiązań.</p>
-                    <p className="about-text">Nasze instalacje współtworzymy razem z Państwem. Każdą zaczynamy od rozmowy z Klientem. Chcemy w pierwszej kolejności poznać Państwa oczekiwania i zapoznać się z projektem Państwa domu. W kolejnym etapie projektujemy instalację i przygotowujemy Państwu proponowane rozwiązania do wyboru. Po Państwa akceptacji, przystępujemy do realizacji.</p>
+                    <p className="about-text">Wspólnie tworzymy dla Państwa rozwiązania, które ograniczają koszty ogrzewania i dbają o nasze wspólne środowisko naturalne.</p>
+                    <p className="about-text bold">Nasze instalacje współtworzymy razem z Państwem</p>
+                    <p className="about-text">1. Zaczynamy od rozmowy z Klientem. Chcemy w pierwszej kolejności poznać Państwa oczekiwania i zapoznać się z projektem Państwa domu</p>
+                    <p className="about-text">2. W kolejnym etapie projektujemy instalację i przygotowujemy Państwu proponowane rozwiązania do wyboru</p>
+                    <p className="about-text">3. Po ich akceptacji, przystępujemy do realizacji</p>
                     <p className="about-text">Nasze rozwiązania opieramy wyłącznie o największych i sprawdzonych producentów w Polsce.</p>
                     <p className="about-text">Rekomendacje naszych dotychczasowych Klientów są potwierdzeniem jakości naszej pracy.</p>
                     <p className="about-text">Zapraszamy do współpracy</p>
