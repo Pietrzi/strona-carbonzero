f11966a42df72cd29f3eb8568f5183d67df39cb6
Pietrzi <pietrzi@op.pl>
2020-12-14 19:15:15 +0100

    playing with this dumb important text

--- strona-carbonz/src/components/Home.js
+++ strona-carbonz/src/components/Home.js
@@ -19,10 +19,12 @@ const Home = () => {
                     <p className="about-text">Od ponad 7 lat CarbonZero promuje rozwiązania przyjazne środowisku i  użytkownikowi.</p>
                     <p className="about-text">Nasz Zespół to LUDZIE z wieloletnim doświadczeniem i ogromną pasją tworzenia nowoczesnych i bezpiecznych rozwiązań.</p>
                     <p className="about-text">Wspólnie tworzymy dla Państwa rozwiązania, które ograniczają koszty ogrzewania i dbają o nasze wspólne środowisko naturalne.</p>
-                    <p className="about-text bold">Nasze instalacje współtworzymy razem z Państwem</p>
-                    <p className="about-text special"><img className="right-arrow" alt="arrow" src={require('../assets/arrow-right-solid.svg')}></img>Zaczynamy od rozmowy z Klientem. Chcemy w pierwszej kolejności poznać Państwa oczekiwania i zapoznać się z projektem Państwa domu</p>
-                    <p className="about-text special"><img className="right-arrow" alt="arrow" src={require('../assets/arrow-right-solid.svg')}></img>W kolejnym etapie projektujemy instalację i przygotowujemy Państwu proponowane rozwiązania do wyboru</p>
-                    <p className="about-text special"><img className="right-arrow" alt="arrow" src={require('../assets/arrow-right-solid.svg')}></img>Po ich akceptacji, przystępujemy do realizacji</p>
+                    <div className="special-text-box">
+                        <p className="about-text bold">Nasze instalacje współtworzymy razem z Państwem</p>
+                        <p className="about-text special"><img className="right-arrow" alt="arrow" src={require('../assets/arrow-right-solid.svg')}></img>Zaczynamy od rozmowy z Klientem. Chcemy w pierwszej kolejności poznać Państwa oczekiwania i zapoznać się z projektem Państwa domu</p>
+                        <p className="about-text special"><img className="right-arrow" alt="arrow" src={require('../assets/arrow-right-solid.svg')}></img>W kolejnym etapie projektujemy instalację i przygotowujemy Państwu proponowane rozwiązania do wyboru</p>
+                        <p className="about-text special"><img className="right-arrow" alt="arrow" src={require('../assets/arrow-right-solid.svg')}></img>Po ich akceptacji, przystępujemy do realizacji</p>
+                    </div>
                     <p className="about-text">Nasze rozwiązania opieramy wyłącznie o największych i sprawdzonych producentów w Polsce.</p>
                     <p className="about-text">Rekomendacje naszych dotychczasowych Klientów są potwierdzeniem jakości naszej pracy.</p>
                     <p className="about-text">Zapraszamy do współpracy</p>
